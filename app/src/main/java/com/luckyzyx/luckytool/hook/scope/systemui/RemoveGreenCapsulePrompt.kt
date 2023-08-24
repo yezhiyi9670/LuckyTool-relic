0ec3f67bc4a9b87ec7fb503613611d8b6cbf0081
luckyzyx <2813467705@qq.com>
2023-08-24 18:45:44 +0800

    - [适配] (Test) 绿色胶囊提示 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/RemoveGreenCapsulePrompt.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/RemoveGreenCapsulePrompt.kt
@@ -1,18 +1,27 @@
 package com.luckyzyx.luckytool.hook.scope.systemui
 
+import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.luckyzyx.luckytool.utils.A14
+import com.luckyzyx.luckytool.utils.SDK
 
 object RemoveGreenCapsulePrompt : YukiBaseHooker() {
     override fun onHook() {
         //Source SystemPromptView
-        findClass("com.oplusos.systemui.statusbar.widget.SystemPromptView").hook {
+        VariousClass(
+            "com.oplusos.systemui.statusbar.widget.SystemPromptView", //C13
+            "com.oplus.systemui.statusbar.widget.SystemPromptView" //C14
+        ).hook {
             injectMember {
                 method { name = "updateViewVisible" }
                 beforeHook { field { name = "disable" }.get(instance).setTrue() }
             }
             injectMember {
-                method { name = "setViewVisibleByDisable" }
-                beforeHook { args(0).setTrue() }
+                method {
+                    name = if (SDK >= A14) "disable"
+                    else "setViewVisibleByDisable"
+                }
+                beforeHook { args().first().setTrue() }
             }
         }
     }
