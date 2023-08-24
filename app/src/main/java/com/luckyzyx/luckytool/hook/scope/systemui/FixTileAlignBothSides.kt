327f5a15ca6b5a5c30ae71de7b2413ed8b739226
luckyzyx <2813467705@qq.com>
2023-08-24 22:34:49 +0800

    - [适配] (Test) 磁贴两侧对齐 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/FixTileAlignBothSides.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/FixTileAlignBothSides.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.luckyzyx.luckytool.utils.ModulePrefs
+import com.luckyzyx.luckytool.utils.getOSVersionCode
 import com.luckyzyx.luckytool.utils.getScreenOrientation
 
 object FixTileAlignBothSides : YukiBaseHooker() {
@@ -15,10 +17,14 @@ object FixTileAlignBothSides : YukiBaseHooker() {
         val columnHorizontal = prefs(ModulePrefs).getInt("tile_columns_horizontal_c13", 4)
         //Sourcee QuickStatusBarHeader 竖屏溢出
         //Search quick_qs_panel -> qs_header_panel_side_padding 24dp
-        findClass("com.android.systemui.qs.QuickStatusBarHeader").hook {
+        VariousClass(
+            "com.android.systemui.qs.QuickStatusBarHeader", //C13.0
+            "com.oplus.systemui.qs.OplusQuickStatusBarHeader" //C14
+        ).hook {
             injectMember {
                 method { name = "updateHeadersPadding" }
                 afterHook {
+                    if (getOSVersionCode > 26) return@afterHook
                     field { name = "mHeaderQsPanel" }.get(instance).cast<LinearLayout>()?.apply {
                         val qsHeaderPanelSidePadding = resources.getDimensionPixelSize(
                             resources.getIdentifier(
@@ -32,7 +38,10 @@ object FixTileAlignBothSides : YukiBaseHooker() {
         }
         //Source QSFragmentHelper 横屏溢出
         //Search expanded_qs_scroll_view -> qs_brightness_mirror_side_padding / qs_bottom_side_padding 24dp
-        findClass("com.oplusos.systemui.qs.helper.QSFragmentHelper").hook {
+        VariousClass(
+            "com.oplusos.systemui.qs.helper.QSFragmentHelper", //C13
+            "com.oplus.systemui.qs.helper.QSFragmentHelper" //C14
+        ).hook {
             injectMember {
                 method { name = "updateQsState" }
                 afterHook {
