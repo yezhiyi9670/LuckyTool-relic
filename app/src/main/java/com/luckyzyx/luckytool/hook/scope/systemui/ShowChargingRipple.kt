6f9d256cce297c4f148c2d60e15342f4bdee3de2
luckyzyx <2813467705@qq.com>
2023-08-25 00:09:15 +0800

    - [适配] (Test) 充电纹波 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/ShowChargingRipple.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/ShowChargingRipple.kt
@@ -1,25 +1,31 @@
 package com.luckyzyx.luckytool.hook.scope.systemui
 
+import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.MembersType
+import com.luckyzyx.luckytool.utils.A14
+import com.luckyzyx.luckytool.utils.SDK
 
 object ShowChargingRipple : YukiBaseHooker() {
     override fun onHook() {
-        //Sourcee FeatureFlags
-        //flag_charging_ripple
-        findClass("com.android.systemui.statusbar.FeatureFlags").hook {
-            injectMember {
-                method { name = "isChargingRippleEnabled" }
-                replaceToTrue()
-            }
-        }
-        //Source WiredChargingRippleController
-        findClass("com.android.systemui.statusbar.charging.WiredChargingRippleController").hook {
+        //Source WiredChargingRippleController -> flag_charging_ripple
+        VariousClass(
+            "com.android.systemui.statusbar.charging.WiredChargingRippleController", //C13
+            "com.android.systemui.charging.WiredChargingRippleController" //C14
+        ).hook {
             injectMember {
                 allMembers(MembersType.CONSTRUCTOR)
                 afterHook {
                     field { name = "rippleEnabled" }.get(instance).setTrue()
                 }
+            }
+        }
+        if (SDK >= A14) return
+        //Sourcee FeatureFlags -> flag_charging_ripple
+        findClass("com.android.systemui.statusbar.FeatureFlags").hook {
+            injectMember {
+                method { name = "isChargingRippleEnabled" }
+                replaceToTrue()
             }
         }
     }
