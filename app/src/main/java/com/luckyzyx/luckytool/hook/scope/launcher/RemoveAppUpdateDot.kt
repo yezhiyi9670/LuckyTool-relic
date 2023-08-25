57f647fb854b493207f4f1e61c5a231a5dcb769c
luckyzyx <2813467705@qq.com>
2023-08-25 15:27:14 +0800

    - [适配] 桌面-移除更新圆点 v14.0.6(1377ee0)

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/launcher/RemoveAppUpdateDot.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/launcher/RemoveAppUpdateDot.kt
@@ -4,17 +4,14 @@ import android.widget.TextView
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.current
 import com.luckyzyx.luckytool.utils.A12
-import com.luckyzyx.luckytool.utils.A121
 import com.luckyzyx.luckytool.utils.A13
 import com.luckyzyx.luckytool.utils.SDK
 
 object RemoveAppUpdateDot : YukiBaseHooker() {
     override fun onHook() {
-        val clazz = when (SDK) {
-            A13 -> "com.android.launcher3.BubbleTextView"
-            A12, A121 -> "com.android.launcher3.OplusBubbleTextView"
-            else -> "com.android.launcher3.OplusBubbleTextView" //A11 C12.0
-        }
+        val clazz = if (SDK >= A13) "com.android.launcher3.BubbleTextView" //C13 C14
+        else if (SDK >= A12) "com.android.launcher3.OplusBubbleTextView" //C12.1
+        else "com.android.launcher3.OplusBubbleTextView" //A11 C12.0
         //Source OplusBubbleTextView
         findClass(clazz).hook {
             injectMember {
@@ -29,8 +26,7 @@ object RemoveAppUpdateDot : YukiBaseHooker() {
                 beforeHook {
                     val itemInfoWithIcon = args().first().any() ?: return@beforeHook
                     val title = itemInfoWithIcon.current().field {
-                        name = "title"
-                        superClass()
+                        name = "title";superClass()
                     }.cast<CharSequence>()
                     instance<TextView>().text = title
                     resultNull()
