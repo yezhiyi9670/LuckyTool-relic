70a911d6018369e3176fa76aba489fd752094a73
luckyzyx <2813467705@qq.com>
2023-09-05 03:24:50 +0800

    - [优化] 横屏磁贴两侧对齐 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/FixTileAlignBothSides.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/FixTileAlignBothSides.kt
@@ -9,55 +9,76 @@ import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.luckyzyx.luckytool.utils.ModulePrefs
 import com.luckyzyx.luckytool.utils.getOSVersionCode
 import com.luckyzyx.luckytool.utils.getScreenOrientation
+import com.luckyzyx.luckytool.utils.safeOfNull
 
 object FixTileAlignBothSides : YukiBaseHooker() {
-    @SuppressLint("DiscouragedApi")
     override fun onHook() {
-        val isCustomTile = prefs(ModulePrefs).getBoolean("control_center_tile_enable", false)
-        val columnHorizontal = prefs(ModulePrefs).getInt("tile_columns_horizontal_c13", 4)
-        //Sourcee QuickStatusBarHeader 竖屏溢出
-        //Search quick_qs_panel -> qs_header_panel_side_padding 24dp
-        VariousClass(
-            "com.android.systemui.qs.QuickStatusBarHeader", //C13.0
-            "com.oplus.systemui.qs.OplusQuickStatusBarHeader" //C14
-        ).hook {
-            injectMember {
-                method { name = "updateHeadersPadding" }
-                afterHook {
-                    if (getOSVersionCode > 26) return@afterHook
-                    field { name = "mHeaderQsPanel" }.get(instance).cast<LinearLayout>()?.apply {
-                        val qsHeaderPanelSidePadding = resources.getDimensionPixelSize(
-                            resources.getIdentifier(
-                                "qs_header_panel_side_padding", "dimen", packageName
-                            )
-                        )
-                        setViewPadding(qsHeaderPanelSidePadding)
+        if (getOSVersionCode <= 26) loadHooker(HookTileAlignVertical)
+        loadHooker(HookTileAlignHorizontal)
+    }
+
+    private object HookTileAlignVertical : YukiBaseHooker() {
+        @SuppressLint("DiscouragedApi")
+        override fun onHook() {
+            //Sourcee QuickStatusBarHeader 竖屏溢出
+            //Search quick_qs_panel -> qs_header_panel_side_padding 24dp
+            findClass("com.android.systemui.qs.QuickStatusBarHeader").hook {
+                injectMember {
+                    method { name = "updateHeadersPadding" }
+                    afterHook {
+                        field { name = "mHeaderQsPanel" }.get(instance).cast<LinearLayout>()
+                            ?.apply {
+                                val qsHeaderPanelSidePadding = safeOfNull {
+                                    resources.getDimensionPixelSize(
+                                        resources.getIdentifier(
+                                            "qs_header_panel_side_padding", "dimen", packageName
+                                        )
+                                    )
+                                } ?: return@afterHook
+                                setViewPadding(qsHeaderPanelSidePadding)
+                            }
                     }
                 }
             }
         }
-        //Source QSFragmentHelper 横屏溢出
-        //Search expanded_qs_scroll_view -> qs_brightness_mirror_side_padding / qs_bottom_side_padding 24dp
-        VariousClass(
-            "com.oplusos.systemui.qs.helper.QSFragmentHelper", //C13
-            "com.oplus.systemui.qs.helper.QSFragmentHelper" //C14
-        ).hook {
-            injectMember {
-                method { name = "updateQsState" }
-                afterHook {
-                    field { name = "mQSPanelScrollView" }.get(instance).cast<ViewGroup>()?.apply {
-                        getScreenOrientation(this) {
-                            if (it) setViewPadding(0) else {
-                                val qsBrightnessMirrorSidePadding = resources.getDimensionPixelSize(
-                                    resources.getIdentifier(
-                                        "qs_brightness_mirror_side_padding", "dimen", packageName
-                                    )
-                                )
-                                if (isCustomTile && columnHorizontal > 4) setViewPadding(
-                                    qsBrightnessMirrorSidePadding
-                                )
+    }
+
+    private object HookTileAlignHorizontal : YukiBaseHooker() {
+        @SuppressLint("DiscouragedApi")
+        override fun onHook() {
+            val isCustomTile = prefs(ModulePrefs).getBoolean("control_center_tile_enable", false)
+            val columnHorizontal = prefs(ModulePrefs).getInt("tile_columns_horizontal_c13", 4)
+
+            //Source QSFragmentHelper 横屏溢出
+            //Search expanded_qs_scroll_view -> qs_brightness_mirror_side_padding / qs_bottom_side_padding 24dp
+            VariousClass(
+                "com.oplusos.systemui.qs.helper.QSFragmentHelper", //C13
+                "com.oplus.systemui.qs.helper.QSFragmentHelper" //C14
+            ).hook {
+                injectMember {
+                    method { name = "updateQsState" }
+                    afterHook {
+                        field { name = "mQSPanelScrollView" }.get(instance).cast<ViewGroup>()
+                            ?.apply {
+                                getScreenOrientation(this) {
+                                    if (it) setViewPadding(0)
+                                    else {
+                                        val qsBrightnessMirrorSidePadding =
+                                            safeOfNull {
+                                                resources.getDimensionPixelSize(
+                                                    resources.getIdentifier(
+                                                        "qs_brightness_mirror_side_padding",
+                                                        "dimen",
+                                                        packageName
+                                                    )
+                                                )
+                                            } ?: return@getScreenOrientation
+                                        if (isCustomTile && columnHorizontal > 4) setViewPadding(
+                                            qsBrightnessMirrorSidePadding
+                                        )
+                                    }
+                                }
                             }
-                        }
                     }
                 }
             }
