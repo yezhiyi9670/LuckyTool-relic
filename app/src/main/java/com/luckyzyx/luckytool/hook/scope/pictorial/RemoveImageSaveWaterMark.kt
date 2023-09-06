58a9cfdd934a7134c8d832d82e33fa1d4b1f7098
luckyzyx <2813467705@qq.com>
2023-09-06 18:34:25 +0800

    - [适配] 乐划锁屏 v40.9.19.1(b57fd84)

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/pictorial/RemoveImageSaveWaterMark.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/pictorial/RemoveImageSaveWaterMark.kt
@@ -16,21 +16,26 @@ object RemoveImageSaveWaterMark : YukiBaseHooker() {
     override fun onHook() {
         //Search ImageSaveManager -> getWaterMaskBitmap -> standard_water_mask_template / high_quality_water_mask_template
         searchClass {
-            from("hi", "fj", "ij", "ej", "cj", "mi", "pi", "ri", "wi").absolute()
+            from(
+                "com.heytap.pictorial.feature.slide.ui.saveconfig",
+                "hi", "fj", "ij", "ej", "cj", "mi", "pi", "ri", "wi"
+            ).absolute()
             field { type = HandlerClass }.count(1)
             field { type = FileClass }.count(1)
             field { type = LongType }.count(1)
             field { type = BooleanType }.count(2)
             field { type = StringClass }.count(3)
             method { param(ContextClass) }.count(1)
-            method {
-                param("com.heytap.pictorial.core.bean.BasePictorialData".toClass())
-                returnType = BooleanType
-            }.count(2)
+            method { emptyParam();returnType = HandlerClass }.count(1)
+            method { returnType = BitmapClass }.count(5)
             method {
                 param(BooleanType, VagueType, BitmapClass, BooleanType)
                 returnType = BitmapClass
             }.count(1)
+            method {
+                param("com.heytap.pictorial.core.bean.BasePictorialData".toClass())
+                returnType = BooleanType
+            }.count(2)
             method {
                 param(StringClass, BitmapClass, BitmapClass, StringClass, BooleanType)
                 returnType = BitmapClass
