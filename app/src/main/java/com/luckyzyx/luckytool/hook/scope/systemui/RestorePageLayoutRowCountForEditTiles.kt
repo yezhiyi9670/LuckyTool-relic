6991b4bb6d43345d1f986b23e34616c10f8b42a9
luckyzyx <2813467705@qq.com>
2023-10-10 11:42:33 +0800

    - [适配] 磁贴编辑页面布局行数 C14

--- app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/RestorePageLayoutRowCountForEditTiles.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/scope/systemui/RestorePageLayoutRowCountForEditTiles.kt
@@ -3,6 +3,7 @@ package com.luckyzyx.luckytool.hook.scope.systemui
 import android.view.View
 import android.widget.LinearLayout
 import androidx.core.view.isVisible
+import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.constructor
 import com.highcapable.yukihookapi.hook.factory.field
@@ -11,7 +12,10 @@ import com.highcapable.yukihookapi.hook.factory.method
 object RestorePageLayoutRowCountForEditTiles : YukiBaseHooker() {
     override fun onHook() {
         //Source OplusQSCustomizer
-        "com.oplusos.systemui.qs.customize.OplusQSCustomizer".toClass().apply {
+        VariousClass(
+            "com.oplusos.systemui.qs.customize.OplusQSCustomizer",  //C13
+            "com.oplus.systemui.qs.customize.OplusQSCustomizer"  //C14
+        ).toClass().apply {
             constructor { paramCount = 2 }.hook {
                 after {
                     field { name = "mMoreFunctionLabel" }.get(instance).cast<View>()
